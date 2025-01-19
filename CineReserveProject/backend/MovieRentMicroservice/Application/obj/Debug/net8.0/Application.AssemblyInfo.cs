3e60693c705be077f492a1d3f509b2ae6ae77186
DiotClement <clement.diot.jr@gmail.com>
2025-01-19 19:51:26 +0100

    feat: add the dockerfile

--- CineReserveProject/backend/MovieRentMicroservice/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
+++ CineReserveProject/backend/MovieRentMicroservice/Application/obj/Debug/net8.0/Application.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Application")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a0b959eee7b2ed64614fdf03fc639cebd6b6b427")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+18bdf7388431f75d6cc6826dc6f0d22760aaf16e")]
 [assembly: System.Reflection.AssemblyProductAttribute("Application")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Application")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
